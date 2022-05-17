38c0dc40c862b1dbe48df583196127add31620b3
Valdoveste <caio.valdoveste@outlook.com.br>
2022-05-17 11:52:25 -0300

    To Do

--- C/leetcode/RotateArray/rotate.c
+++ C/leetcode/RotateArray/rotate.c
@@ -1,85 +1,71 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void invertArray(int *nums, int numsSize);
 void rotate(int *nums, int numsSize, int k);
 
 int main()
 {
 
-    int nums[] = {1, 2, 3, 4, 5, 6, 7};
+    int nums[] = {1,2,3};
     int numsSize = (sizeof(nums) / sizeof(nums[0]));
     int k = 3;
 
     rotate(nums, numsSize, k);
 
-    // int i = 0;
-    // while (i < numsSize)
-    // {
-    //     printf("%d, ", nums[i]);
-    //     i++;
-    // }
+    int i = 0;
+    while (i < numsSize)
+    {
+        printf("%d, ", nums[i]);
+        i++;
+    }
 
     return 0;
 }
 
 void rotate(int *nums, int numsSize, int k)
 {
-    int j = 0;
-    int i = 0;
-    int p = (numsSize - k);
-    int aux = 0;
-    int y = 0;
-    // int arrAux[numsSize];
+    if((numsSize - 1) == k)
+        invertArray(nums, numsSize);
 
-    while (i < ((numsSize - k) / 2))
+    if ((numsSize - 1) >= 1 && k < (numsSize))
     {
-        aux = nums[(numsSize - (k + 1)) - i];
-        nums[(numsSize - (k + 1)) - i] = nums[i];
+        int j = 0;
+        int p = (numsSize - k);
+        int i = 0;
+        int aux = 0;
+
+        while ((i < ((numsSize - k) / 2)))
+        {
+            aux = nums[(numsSize - (k + 1)) - i];
+            nums[(numsSize - (k + 1)) - i] = nums[i];
+            nums[i] = aux;
+            i++;
+        }
+
+        while ((p < (numsSize - 1)))
+        {
+            aux = nums[(numsSize - 1) - j];
+            nums[((numsSize - 1) - j)] = nums[p];
+            nums[p] = aux;
+            p++;
+            j++;
+        }
+        invertArray(nums, numsSize);
+    }
+}
+
+void invertArray(int *nums, int numsSize)
+{
+    int i = 0;
+    int aux = 0;
+
+    while ((i < (numsSize / 2)))
+    {
+        aux = nums[((numsSize - 1) - i)];
+        nums[((numsSize - 1) - i)] = nums[i];
         nums[i] = aux;
         i++;
-
-        while (y < numsSize)
-        {
-            printf("%d, ", nums[y]);
-            y++;
-        }
-        printf("\n");
-        y = 0;
-    }
-
-    printf("\n\n");
-    i = 0;
-    while (p < (numsSize - 1))
-    {
-        aux = nums[(numsSize - 1) - i];
-        nums[((numsSize - 1) - i)] = nums[p];
-        nums[p] = aux;
-        p++;
-        i++;
-
-        while (y < numsSize)
-        {
-            printf("%d, ", nums[y]);
-            y++;
-        }
-        printf("\n");
-    }
-
-    printf("\n\n");
-    while (j < (numsSize / 2))
-    {
-        aux = nums[((numsSize - 1) - j)];
-        nums[((numsSize - 1) - j)] = nums[j];
-        nums[j] = aux;
-        j++;
-
-        while (y < numsSize)
-        {
-            printf("%d, ", nums[y]);
-            y++;
-        }
-        printf("\n");
-        y = 0;
     }
 }
 
@@ -139,37 +125,63 @@ void rotate(int *nums, int numsSize, int k)
 //     }
 // }
 
-void rotate(int *nums, int numsSize, int k)
-{
-    int j = 0;
-    int i = 0;
-    int p = (numsSize - k);
-    int aux = 0;
-    int y = 0;
+// void rotate(int *nums, int numsSize, int k)
+// {
+//     int j = 0;
+//     int i = 0;
+//     int p = (numsSize - k);
+//     int aux = 0;
+//     int y = 0;
+//     // int arrAux[numsSize];
 
-    while (i < ((numsSize - k) / 2))
-    {
-        aux = nums[(numsSize - (k + 1)) - i];
-        nums[(numsSize - (k + 1)) - i] = nums[i];
-        nums[i] = aux;
-        i++;
-    }
+//     while (i < ((numsSize - k) / 2))
+//     {
+//         aux = nums[(numsSize - (k + 1)) - i];
+//         nums[(numsSize - (k + 1)) - i] = nums[i];
+//         nums[i] = aux;
+//         i++;
 
-    i = 0;
-    while (p < (numsSize - 1))
-    {
-        aux = nums[(numsSize - 1) - i];
-        nums[((numsSize - 1) - i)] = nums[p];
-        nums[p] = aux;
-        p++;
-        i++;
-    }
+//         while (y < numsSize)
+//         {
+//             printf("%d, ", nums[y]);
+//             y++;
+//         }
+//         printf("\n");
+//         y = 0;
+//     }
 
-    while (j < (numsSize / 2))
-    {
-        aux = nums[((numsSize - 1) - j)];
-        nums[((numsSize - 1) - j)] = nums[j];
-        nums[j] = aux;
-        j++;
-    }
-}
+//     printf("\n\n");
+//     i = 0;
+//     while (p < (numsSize - 1))
+//     {
+//         aux = nums[(numsSize - 1) - i];
+//         nums[((numsSize - 1) - i)] = nums[p];
+//         nums[p] = aux;
+//         p++;
+//         i++;
+
+//         while (y < numsSize)
+//         {
+//             printf("%d, ", nums[y]);
+//             y++;
+//         }
+//         printf("\n");
+//     }
+
+//     printf("\n\n");
+//     while (j < (numsSize / 2))
+//     {
+//         aux = nums[((numsSize - 1) - j)];
+//         nums[((numsSize - 1) - j)] = nums[j];
+//         nums[j] = aux;
+//         j++;
+
+//         while (y < numsSize)
+//         {
+//             printf("%d, ", nums[y]);
+//             y++;
+//         }
+//         printf("\n");
+//         y = 0;
+//     }
+// }
